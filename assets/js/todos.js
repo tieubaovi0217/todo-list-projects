68c23c0a1cba14d9163dd08650a917db664dab34
Tieu Bao <47345262+tieubaovi0217@users.noreply.github.com>
2020-03-27 10:49:49 +0700

    Add some jQuery

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,30 @@
-// alert("CONNECTED!!!");
+// Check off Specific todo by clicking
+$('ul').on("click", "span.item-name", function(){
+    $(this).toggleClass('completed');
+});
+
+// Click on trash to delete todo
+$('ul').on("click", "span.trash", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+// Add a new todo
+$('input[type="text"]').keypress(function(event){
+    if(event.which == 13) {
+        // get text form input
+        var text = $(this).val();
+        // create a new li and add to ul
+        var newLi = '<li><span class="trash"><i class="fa fa-trash"></i></span>'
+         + '<span class="item-name">' + text + '</span></li>';
+        $('ul').append(newLi);
+        $(this).val("");
+    }
+});
+
+// Toggle the input form
+$('.fa-plus').click(function(){
+    $('input[type="text"]').fadeToggle(500);
+});
